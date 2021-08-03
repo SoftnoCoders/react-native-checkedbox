8630e21f29980738ae0ea62e5e2956945be300fa
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-03 18:45:01 +0500

    Added PropTypes & onPress passing Value to Function!

--- CheckBox.js
+++ CheckBox.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 
-const App = props => { // props: value | onPress | disabled | tickColor | backgroundColor 
+const CheckBox = props => {
   return (
-      <TouchableOpacity style={styles.touchableOpacity} onPress={props?.onPress} disabled={props?.disabled}>
+      <TouchableOpacity style={styles.touchableOpacity} onPress={props?.onPress.bind(null, !props?.value)} disabled={props?.disabled}>
         <View style={[styles.backLayer, {backgroundColor: props?.value ? 'rgba(42, 187, 155, 0.1)' : 'transparent'}]} />
         <View style={[styles.container, { backgroundColor: props?.value ? (props?.backgroundColor ? props?.backgroundColor : 'blue') : 'transparent', borderColor: props?.backgroundColor ? props?.backgroundColor : 'blue'  }]}>
-          { props?.value && ( <Image source={require('./tick.png')} style={[{tintColor: props?.tickColor ? props.tickColor: 'white'} ]} resizeMode={'cover'} /> )}
+          { props?.value && ( <Image source={require('./assets/tick2.png')} style={[{tintColor: props?.tickColor ? props.tickColor: 'white'} ]} resizeMode={'cover'} /> )}
         </View>
       </TouchableOpacity>
   );
 }
 
-export default App;
+export default CheckBox;
 
 const styles = StyleSheet.create({
   container: {
@@ -25,17 +26,27 @@ const styles = StyleSheet.create({
     borderRadius: 2,
   },
   touchableOpacity: {
-    width: 32, 
-    height: 32, 
+    width: 35, 
+    height: 33, 
     justifyContent: 'center', 
     alignItems: 'center'
+    // borderWidth: 1
   },
   backLayer: {
     position: 'absolute', 
-    top: 4, 
-    left: 4, 
+    top: 4.5, 
+    left: 6, 
     width: 24, 
     height: 24, 
     borderRadius: 3,
   }
 })
+
+CheckBox.propTypes = {
+  value: PropTypes.bool,
+  onPress: PropTypes.func,
+  disabled: PropTypes.bool,
+  tickColor: PropTypes.string,
+  backgroundColor: PropTypes.string
+}
+
